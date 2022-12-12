1339bc3d9af28b85553a83c68e0a75a3bec1a89e
liz-fw <lstebbins@flowwest.com>
2022-12-12 13:07:38 -0800

    writes new csv files and deletes old csv files to data/

--- data-raw/clean_data.R
+++ data-raw/clean_data.R
@@ -10,23 +10,28 @@ library(readxl)
 
 # catch
 catch <- read_xlsx(here::here("data-raw", "feather_catch_edi.xlsx")) |> glimpse()
+write_csv(catch, here::here("data","feather_catch_edi.csv"))
 
 # trap
 trap <- read_xlsx(here::here("data-raw", "feather_trap_edi.xlsx")) |>
   glimpse()
+write_csv(trap, here::here("data","feather_trap_edi.csv"))
 # note: waterVel column is 99.7% NAs, discharge column is 75.8% NAs
 
 # recapture
 recapture <- read_xlsx(here::here("data-raw", "feather_recaptures_edi.xlsx")) |>
   glimpse()
+write_csv(recapture, here::here("data","feather_recaptures_edi.csv"))
 
 # release
 releases <- read_xlsx(here::here("data-raw", "feather_releases_edi.xlsx")) |>
   glimpse()
+write_csv(releases, here::here("data","feather_releases_edi.csv"))
 
 # release fish
 # per butte edi example, don't need to upload this right now but keeping available for
 # the future
 release_fish <- read_xlsx(here::here("data-raw", "feather_releasefish_edi.xlsx")) |>
   glimpse()
+write_csv(release_fish, here::here("data","feather_releasefish_edi.csv"))
 
