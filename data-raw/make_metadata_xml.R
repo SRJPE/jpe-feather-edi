dcccc748504b988e981c72e6137fd793c4662ab6
InigoP <inigop@rogers.com>
2024-01-30 15:37:14 -0800

    updates to dockerfile

--- data-raw/make_metadata_xml.R
+++ data-raw/make_metadata_xml.R
@@ -37,7 +37,7 @@ methods_docx <- "data-raw/metadata/methods.md"
 catch_df <- readr::read_csv("data/feather_catch.csv")
 catch_coverage <- tail(catch_df$visitTime, 1)
 metadata$coverage$end_date <- lubridate::floor_date(catch_coverage, unit = "days")
-write.xlsx(metadata$coverage, sheetName="coverage", row.names = FALSE)
+openxlsx::write.xlsx(metadata$coverage, excel_path, sheetName="coverage", rowNames = FALSE)
 #edi_number <- reserve_edi_id(user_id = Sys.getenv("EDI_USER_ID"), password = Sys.getenv("EDI_PASSWORD"))
 # edi_number <- "edi.1239.2"
 
